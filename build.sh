eb9a08dc1a8eec583cdc1ca64a79141cb718c1f2
Lucas <lucaslls.1989@hotmail.com>
2026-09-03 18:54:40 -0300

    corrigindo para usuario

--- build.sh
+++ build.sh
@@ -7,3 +7,28 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py shell <<'PY'
+import os
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email or "",
+            password=password
+        )
+        print("Superusuário criado com sucesso.")
+    else:
+        print("Superusuário já existe. Nenhuma alteração necessária.")
+else:
+    print("Variáveis do superusuário não configuradas.")
+PY
